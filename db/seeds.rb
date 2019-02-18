89953cd3d9d84a6003bf77cac07e5778ac59fe52
Deborah Marino <heima.heima.creative@gmail.com>
2019-02-18 18:34:02 +0100

    branch seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(first_name: 'Laurent', last_name: 'Korvisy', email: 'laurent.korvisy@gmail.com', phone_number:'0631233432', description: '', owner: false, picture: "" )
+User.create(first_name: 'Thomas', last_name: 'Morin', email: 'thomas.morin@gmail.com', phone_number:'0631263432', description: "")
+User.create(first_name: 'Charlotte', last_name: 'Goyard', email: 'charlotte.goyard@gmail.com', phone_number:'0631256432')
+User.create(first_name: 'Maxime', last_name: 'Carneiro', email: 'maxime.carneiro@gmail.com', phone_number:'0633433432')
+User.create(first_name: 'Calvin', last_name: 'Ponsot', email: 'calvin.ponsot@gmail.com', phone_number:'0631233476')
+User.create(first_name: 'Rémi', last_name: 'Falcoz', email: 'remi.falcoz@gmail.com', phone_number:'0631233456')
+User.create(first_name: 'Chloé', last_name: 'Rouby', email: 'chloe.rouby@gmail.com', phone_number:'0631235678')
+User.create(first_name: 'Bastien', last_name: 'Pasquier', email: 'bastien.pasquier@gmail.com', phone_number:'0631245673')
+User.create(first_name: 'François', last_name: 'Gonnet', email: 'francois.gonnet@gmail.com', phone_number:'0631569034')
+User.create(first_name: 'Charlène', last_name: 'Haskis', email: 'charlene.haskis@gmail.com', phone_number:'0631548709')
