b75df16a959dd8d37b5efc7c4f7d48c2333c43c6
Maxime Berger <berger.pro.maxime@gmail.com>
2019-02-19 17:29:19 +0100

    added show and index to cabin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cabins, only: :show do
+  resources :cabins, only: [:show, :index] do
     resources :bookings, only: [:new, :create]
     collection do
       get 'top'
