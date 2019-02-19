c870519e772f0df87ec77d49a3ba17cfaaccebae
Maxime Berger <berger.pro.maxime@gmail.com>
2019-02-19 10:54:27 +0100

    routes V1

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cabins, only: :show do
+    resources :bookings, only: [:new, :create]
+    collection do
+      get 'top'
+      get 'destination'
+      get 'capacity'
+    end
+  end
+
+  resources :bookings, only: :show
+
+  resources :user, only: :show do
+    resources :owned_cabins
+  end
+
 
 end
