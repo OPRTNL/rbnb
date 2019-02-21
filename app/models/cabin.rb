f10255b590a371df372c6657573b5f11a829d1df
Longchamp <glongchamp8@gmail.com>
2019-02-21 14:39:00 +0100

    change model cabin -rating- and update cabin new form

--- app/models/cabin.rb
+++ app/models/cabin.rb
@@ -6,19 +6,22 @@ class Cabin < ApplicationRecord
   validates :name, presence: true
   validates :picture, presence: true
   validates :daily_rate, presence: true
-  validates :capacity, presence: true
+  # validates :capacity, presence: true
 
   def rating
   ratings = []
   bookings.each do |booking|
     ratings << booking.rating
   end
-  p ratings.sum.fdiv(ratings.length)
-  return ratings.sum.fdiv(ratings.length)
+  # if ratings.sum.fdiv(ratings.length)
+  #   ratings.sum.fdiv(ratings.length)
+  # else
+  #   "0"
+  # end
   end
 
-  def all_booked_nights
-    # bookings = self.bookings
-    self.bookings.pluck(:check_in, :check_out).map { |b| (b[0]..b[1]).to_a }.flatten.uniq.sort
-  end
+  # def all_booked_nights
+  #   # bookings = self.bookings
+  #   self.bookings.pluck(:check_in, :check_out).map { |b| (b[0]..b[1]).to_a }.flatten.uniq.sort
+  # end
 end
