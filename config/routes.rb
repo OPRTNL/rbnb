ce5e5c21f2fdbfa3195fc0b22fb4258764c8a5e4
Longchamp <glongchamp8@gmail.com>
2019-02-21 13:24:14 +0100

    routes change for cabins

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cabins, only: [:show, :index] do
+  resources :cabins, only: [:show, :index, :new, :create] do
     resources :bookings, only: [:new, :create]
     collection do
       get 'top'
@@ -13,10 +13,7 @@ Rails.application.routes.draw do
 
   resources :bookings, only: :show
 
-
-  resources :users, only: [:show, :edit, :update] do
-    resources :owned_cabins
-  end
+  resources :users, only: [:show, :edit, :update]
 
   resources :bookings, only: [:index]
 
