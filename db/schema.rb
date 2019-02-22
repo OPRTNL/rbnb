2babf9981cb6e9c06629c7b0581915e7d650ce4a
Deborah <heima.heima.creative@gmail.com>
2019-02-22 12:11:51 +0100

    front front

--- db/schema.rb
+++ db/schema.rb
@@ -68,10 +68,6 @@ ActiveRecord::Schema.define(version: 2019_02_22_084912) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "avatar_file_name"
-    t.string "avatar_content_type"
-    t.integer "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
